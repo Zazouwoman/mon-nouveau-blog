86a8115b16046e7be06ceb6507992b8f74ade87f
Zazouwoman <gaudy.claire@gmail.com>
2022-10-23 09:18:12 +0200

    Signed-off-by: Zazouwoman <gaudy.claire@gmail.com>

--- bdd/models.py
+++ bdd/models.py
@@ -807,7 +807,8 @@ class InfoEmail(models.Model):
 class Attachment(models.Model):
     message = models.ForeignKey(InfoEmail, verbose_name=_('InfoEmail'), on_delete=models.CASCADE)
     nom = models.CharField(max_length =70, blank = True)
-    file = models.FileField(_('Attachment'), upload_to='attachments')
+    #file = models.FileField(_('Attachment'), upload_to='attachments')
+    file = models.FileField(_('Attachment'), upload_to=''))
 
     def selflink(self):
         if self.id:
